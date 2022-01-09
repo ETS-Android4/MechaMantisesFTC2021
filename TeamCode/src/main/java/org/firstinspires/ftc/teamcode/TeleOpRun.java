4b4d0f1b781816a13c8801e7adcc77bcf54650e5
AJain862 <72466106+AJain862@users.noreply.github.com>
2022-01-09 15:24:25 -0800

    Final Fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRun.java
@@ -1,123 +1,228 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 @TeleOp(name = "TeleOp!")
-public class TeleOpRun extends LinearOpMode {
-    public void runOpMode(){
-        //int rotations = 0;
-        boolean claw_close;
-        boolean claw_open;
-        double left;
-        double right;
-        boolean arm_up;
-        boolean arm_down;
-        boolean arm_middle;
-        boolean arm_off;
-        boolean carousel_forward;
-        boolean carousel_stop;
-        boolean carousel_reverse;
-        boolean forward;
-        boolean backward;
-        boolean left_turn;
-        boolean right_turn;
-        MantisesClass mantisClass = new MantisesClass(this); //this does the init
+public class TeleOpRun extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    DcMotor lineararm;
+    Servo intakebox;
+    DcMotor intake;
+    Servo rotatearm;
+    DcMotor fl_wheel;
+    DcMotor fr_wheel;
+    DcMotor bl_wheel;
+    DcMotor br_wheel;
+    DcMotor carousel_arm;
+    DcMotor rotateintake;
+    DcMotor.RunMode encoder_true = DcMotor.RunMode.RUN_USING_ENCODER;
+    DcMotor.RunMode encoder_false = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+    DcMotor.RunMode reset_encoder = DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+    DcMotor.Direction direction_forward = DcMotor.Direction.FORWARD;
+    DcMotor.Direction direction_reverse = DcMotor.Direction.REVERSE;
+    @Override
+    public void init(){
+        telemetry.addData("Initializing", "...");
+        telemetry.update();
 
-        while (!opModeIsActive() && !isStopRequested()) {//you can just use waitForStart() here, but this prints stuff out, you guys don't have this so it would be running while you have not hit the start button which is illegal
-            telemetry.addData("status", "waiting for start command...");
-            telemetry.update();
+
+
+        lineararm = hardwareMap.get(DcMotor.class, "linear");
+
+        intakebox = hardwareMap.get(Servo.class, "intakebox");
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        rotatearm = hardwareMap.get(Servo.class, "rarm");
+
+        carousel_arm = hardwareMap.get(DcMotor.class, "carousel");
+
+        rotateintake = hardwareMap.get(DcMotor.class, "ri");
+
+        fl_wheel = hardwareMap.get(DcMotor.class, "fl");
+        fr_wheel = hardwareMap.get(DcMotor.class, "fr");
+        bl_wheel = hardwareMap.get(DcMotor.class, "bl");
+        br_wheel = hardwareMap.get(DcMotor.class, "br");
+
+        intake.setDirection(direction_reverse);
+        intake.setMode(encoder_false);
+
+        lineararm.setDirection(direction_forward);
+        lineararm.setMode(reset_encoder);
+        lineararm.setMode(encoder_true);
+        lineararm.setPower(0);
+        lineararm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        fl_wheel.setDirection(direction_reverse);
+        fl_wheel.setMode(reset_encoder);
+        fl_wheel.setMode(encoder_true);
+        fl_wheel.setPower(0);
+        fl_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        fr_wheel.setDirection(direction_forward);
+        fr_wheel.setMode(reset_encoder);
+        fr_wheel.setMode(encoder_true);
+        fr_wheel.setPower(0);
+        fr_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        bl_wheel.setDirection(direction_reverse);
+        bl_wheel.setMode(reset_encoder);
+        bl_wheel.setMode(encoder_true);
+        bl_wheel.setPower(0);
+        bl_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        br_wheel.setDirection(direction_forward);
+        br_wheel.setMode(reset_encoder);
+        br_wheel.setMode(encoder_true);
+        br_wheel.setPower(0);
+        br_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        carousel_arm.setDirection(direction_forward);
+        carousel_arm.setMode(reset_encoder);
+        carousel_arm.setMode(encoder_true);
+        carousel_arm.setPower(0);
+        carousel_arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rotateintake.setDirection(direction_reverse);
+        rotateintake.setMode(reset_encoder);
+        rotateintake.setMode(encoder_true);
+        rotateintake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rotateintake.setPower(0);
+
+        rotateintake.setTargetPosition((1440/4)/2);
+        rotateintake.setPower(0.3);
+        rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(rotateintake.isBusy()){
+
         }
-        while(!isStopRequested()){
-            left = -gamepad1.left_stick_y;
-            right = -gamepad1.right_stick_y;
-            claw_close = gamepad1.x&&mantisClass.getServoPos(mantisClass.crane_claw)>0;
-            claw_open = gamepad1.b&&mantisClass.getServoPos(mantisClass.crane_claw)<0.4;
-            carousel_forward = gamepad1.left_bumper;
-            carousel_stop = gamepad1.right_bumper;
-            carousel_reverse = gamepad1.start;
-            arm_up = gamepad2.y/*&&rotations<mantisClass.max_arm*/;
-            arm_down = gamepad2.a/*&&rotations>mantisClass.min_arm*/;
-            arm_middle = gamepad2.x;
-            arm_off = gamepad2.b;
-            forward = gamepad1.dpad_up;
-            backward = gamepad1.dpad_down;
-            left_turn = gamepad1.dpad_left;
-            right_turn = gamepad1.dpad_right;
-            if(claw_open){
-                if(mantisClass.startPosition<0.4){
-                    mantisClass.startPosition+=mantisClass.speed;
-                }
-                mantisClass.crane_claw.setPosition(mantisClass.startPosition+= mantisClass.speed);
-            }else if (claw_close) {
-                mantisClass.crane_claw.setPosition(mantisClass.startPosition -= mantisClass.speed);
-            }
-            if (left==0&&right==0){
-                if(forward){
-                    mantisClass.left_wheel.setPower(0.3);
-                    mantisClass.right_wheel.setPower(0.3);
-                }else if(backward){
-                    mantisClass.left_wheel.setPower(-0.3);
-                    mantisClass.right_wheel.setPower(-0.3);
-                }else if(left_turn){
-                    mantisClass.left_wheel.setPower(-0.3);
-                    mantisClass.right_wheel.setPower(0.3);
-                }else if(right_turn){
-                    mantisClass.left_wheel.setPower(0.3);
-                    mantisClass.right_wheel.setPower(-0.3);
-                }
-                else{
-                    mantisClass.left_wheel.setPower(0);
-                    mantisClass.right_wheel.setPower(0);
-                }
-            }else{
-                mantisClass.left_wheel.setPower(left);
-                mantisClass.right_wheel.setPower(right);
-            }
-            if (arm_up){
-                mantisClass.crane_arm.setTargetPosition(mantisClass.up_arm);
-                mantisClass.crane_arm.setPower(0.1);
-                mantisClass.crane_arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                if(rotations <365){
-//                    rotations+=1;
-//                }
-//                mantisClass.crane_arm.setTargetPosition(rotations+=1);
-//                mantisClass.crane_arm.setPower(0.1);
-//                mantisClass.crane_arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-               // mantisClass.runCraneArm(400, 0.1);
 
-            }else if (arm_down){
-                mantisClass.crane_arm.setTargetPosition(mantisClass.down_arm);
-                mantisClass.crane_arm.setPower(0.1);
-                mantisClass.crane_arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                if (rotations > 0) {
-//                    rotations -= 1;
-//                }
-//                mantisClass.crane_arm.setTargetPosition(rotations-=1);
-//                mantisClass.crane_arm.setPower(0.1);
-//                mantisClass.crane_arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-               // mantisClass.runCraneArm(100, 0.1);
-            }else if(arm_middle) {
-                mantisClass.crane_arm.setTargetPosition(mantisClass.middle_arm);
-                mantisClass.crane_arm.setPower(0.1);
-                mantisClass.crane_arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                //mantisClass.runCraneArm(275, 0.1);
-            }
-            else if(arm_off){
-                mantisClass.crane_arm.setTargetPosition(mantisClass.reset_arm);
-                mantisClass.crane_arm.setPower(0.1);
-                mantisClass.crane_arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-//                mantisClass.runCraneArm(0, 0.1);
-//                mantisClass.crane_arm.setPower(0);
-            }
-            if (carousel_forward){
-                mantisClass.carousel.setPower(1);
-            }else  if(carousel_stop){
-                mantisClass.carousel.setPower(0);
-            }else if(carousel_reverse){
-                mantisClass.carousel.setPower(-1);
-            }else {}
+        rotatearm.setPosition(0.55);
+        intakebox.setPosition(1);
+
+
+        rotateintake.setTargetPosition(0);
+        rotateintake.setPower(0.1);
+        rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (rotateintake.isBusy()){
+
         }
-        stop();//stops the program
+        rotateintake.setPower(0);
+
     }
+    @Override
+    public void init_loop(){
+        telemetry.addData("Initialized", "Waiting for Start");
+
+    }
+    @Override
+    public void start(){
+        runtime.reset();
+    }
+    @Override
+    public void loop(){
+        if(gamepad2.y) {
+            lineararm.setTargetPosition((1440*3)-450);
+            lineararm.setPower(1);
+            lineararm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        else if(gamepad2.a) {
+            lineararm.setTargetPosition(0);
+            lineararm.setPower(1);
+            lineararm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        else if(gamepad2.b){
+            intakebox.setPosition(0.4);
+
+        }
+        else if(gamepad1.right_bumper){
+            carousel_arm.setPower(-1);
+        }else if(gamepad1.left_bumper){
+            carousel_arm.setPower(0);
+        }
+        else if (gamepad2.right_bumper){
+            rotateintake.setTargetPosition((1440/4)/2);
+            rotateintake.setPower(0.3);
+            rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while (rotateintake.isBusy()){
+
+            }
+            intakebox.setPosition(0.35);
+            rotateintake.setTargetPosition(0);
+            rotateintake.setPower(0.3);
+            rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while (rotateintake.isBusy()){
+
+            }
+            rotateintake.setPower(0);
+            intake.setPower(-1);
+
+
+        }else if(gamepad2.left_bumper){
+            intake.setPower(0);
+            rotateintake.setTargetPosition(90);
+            rotateintake.setPower(0.3);
+            rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            intakebox.setPosition(0.5);
+            intakebox.setPosition(1);
+            rotateintake.setTargetPosition(0);
+            rotateintake.setPower(0.1);
+            rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }else if(gamepad2.dpad_left){
+            rotatearm.setPosition(0);
+        }else if(gamepad2.dpad_right){
+            rotatearm.setPosition(1);
+        }else if (gamepad2.dpad_up){
+            rotatearm.setPosition(0.55);
+        }else if(gamepad2.x){
+            intakebox.setPosition(1);
+        }else if(gamepad2.dpad_down){
+            rotateintake.setTargetPosition((1440/4)/2);
+            rotateintake.setPower(0.3);
+            rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while(rotateintake.isBusy()){
+
+            }
+
+
+            rotatearm.setPosition(0.55);
+            intakebox.setPosition(0.4);
+
+
+            rotateintake.setTargetPosition(0);
+            rotateintake.setPower(0.1);
+            rotateintake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while (rotateintake.isBusy()){
+
+            }
+            rotateintake.setPower(0);
+
+        }
+        double y = -gamepad1.right_stick_y;
+        double x = gamepad1.right_stick_x ;
+        double rx = gamepad1.left_stick_x;
+
+        double denominator = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(rx), 0.5);
+        double frontLeftPower = (y+x+rx)/denominator;
+        double backLeftPower = (y-x+rx)/denominator;
+        double frontRightPower = (y-x-rx)/denominator;
+        double backRightPower = (y+x-rx)/denominator;
+
+        fl_wheel.setPower(frontLeftPower/2);
+        bl_wheel.setPower(backLeftPower/2);
+        fr_wheel.setPower(frontRightPower/2);
+        br_wheel.setPower(backRightPower/2);
+
+
+
+    }
+    @Override
+    public void stop(){
+
+    }
+
 }
